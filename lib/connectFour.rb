705caae4e2b469ea7d391a1d6340d0fff1744194
Shaswat Satyam <70892928+shaswat-satyam@users.noreply.github.com>
2024-04-23 09:23:12 +0530

    Completed the Connect four

--- lib/connectFour.rb
+++ lib/connectFour.rb
@@ -1,28 +1,32 @@
-
 class ConnectFour
   attr_accessor :board, :top
-  def initialize()
-    @board = Array.new(6){Array.new(7)}
-    @top = Array.new(7){0}
+
+  def initialize
+    @board = Array.new(6) { Array.new(7) }
+    @top = Array.new(7) { 0 }
   end
 
   def display
-    @board.each do |rows| 
-      rows.each{|cell| print cell}
+    @board.each do |rows|
+      rows.each { |cell| print cell }
       puts
-     end
-   end
-  
+    end
+  end
+
   def isFull?
-    @board.flatten.all?{|cell| !cell.nil?}
+    @board.flatten.all? { |cell| !cell.nil? }
   end
 
   def validMove?(col)
-    -1 < col and col < 8 and top[col] < 7
+    return false unless -1 < col and col < 6
+    return false unless top[col] < 7
+
+    true
   end
-  def newMove(col,player)
-    if(!validMove?(col))
-      puts "You entered "+col.to_s+" which is not valid!"
+
+  def newMove(col, player)
+    unless validMove?(col)
+      puts 'You entered ' + col.to_s + ' which is not valid!'
       return
     end
     @board[col][top[col]] = player
@@ -30,70 +34,70 @@ class ConnectFour
   end
 
   def display
-    
     @board.first.length.times do |j|
-
       @board.length.times do |i|
-        ele = @board[i][@board.first.length - 1- j]
-    if ele.nil?
-        print "O  "
-      else
-          if ele == 0
-            print "\u{1f534} "
+        ele = @board[i][@board.first.length - 1 - j]
+        if ele.nil?
+          print 'O  '
+        elsif ele == 0
+          print "\u{1f534} "
         else
-            print "\u{1f7e2} "
-          end
+          print "\u{1f7e2} "
         end
-    
-    end
-    puts 
+      end
+      puts
     end
   end
 
   def hasWon(player)
-    verticalWin(player)|horizontalWin(player)|diagonalWin(player)
+    verticalWin(player) | horizontalWin(player) | diagonalWin(player)
   end
 
   def verticalWin(player)
     @board.any? do |row|
-      row.each_slice(4).to_a.any?{|sub| sub.all?{|ele| ele == player}}
+      row.each_slice(4).to_a.any? { |sub| sub.all? { |ele| ele == player } }
     end
   end
-
 
   def diagonalWin(player)
-  4.times do |i|
-    @board[0].length.times do |j|
-        puts i.to_s + " " + j.to_s
-      return true if 4.times.all? {|k| @board[i+k][j+k] == player or @board[i-k][j-k] == player or @board[i+k][j-k] == player or @board[i-k][j+k] == player} 
+    3.times do |i|
+      4.times do |j|
+        return true if [@board[0 + i][3 - j], @board[1 + i][4 - j], @board[2 + i][5 - j],
+                        @board[3 + i][6 - j]].all? do |ele|
+                         ele == player
+                       end
+        return true if [@board[5 - j][3 - i], @board[4 - j][4 - i], @board[3 - j][5 - i],
+                        @board[2 - j][6 - i]].all? do |ele|
+                         ele == player
+                       end
+      end
     end
+    false
   end
-  false
-end
 
   def horizontalWin(player)
     @board.transpose.any? do |row|
-      row.each_slice(4).to_a.any?{|sub| sub.all?{|ele| ele == player}}
+      row.each_slice(4).to_a.any? { |sub| sub.all? { |ele| ele == player } }
     end
-  end  
+  end
 
   def game
     player = 0
     loop do
-      system("cls") || system("clear")
+      system('cls') || system('clear')
       display
-      puts "Player "+ player.to_s + " Enter your Move (0-5)"
+      puts 'Player ' + player.to_s + ' Enter your Move (0-5)'
       input = gets.chomp.to_i
-      newMove(input,player)
+      newMove(input, player)
       break if hasWon(player)
-      player = (1+player)%2
+
+      player = (1 + player) % 2
     end
     system('clear')
     display
-  puts "Player " + player.to_s+ " has won!"
+    puts 'Player ' + player.to_s + ' has won!'
   end
 end
-
 
 game1 = ConnectFour.new
 game1.game
--- spec/connectFour_spec.rb
+++ spec/connectFour_spec.rb
@@ -1,110 +1,107 @@
 require 'connectFour'
 describe ConnectFour do
-  subject(:newGame) {described_class.new  }
+  subject(:newGame) { described_class.new }
 
-  it 'can be initalized'do
+  it 'can be initalized' do
     expect(newGame).not_to be_nil
   end
 
-  describe '#isFull?' do 
-    it 'check if the board is empty'do
+  describe '#isFull?' do
+    it 'check if the board is empty' do
       expect(newGame.isFull?).to be false
-     end
-    
+    end
+
     it 'ends the game if board is full' do
-      7.times do |i|
-        6.times{|j| newGame.newMove(j,0)}
+      7.times do |_i|
+        6.times { |j| newGame.newMove(j, 0) }
       end
       expect(newGame.isFull?).to be true
-
     end
   end
 
-   
-  describe "#newMove" do
-    it 'accepts input from player'do
-      newGame.newMove(1,1)
-    expect(newGame.board[1][0]).to eq(1)
+  describe '#newMove' do
+    it 'accepts input from player' do
+      newGame.newMove(1, 1)
+      expect(newGame.board[1][0]).to eq(1)
     end
-    it 'accepts two moves from player0'do
-    newGame.newMove(1,0)
-    newGame.newMove(1,0)
+    it 'accepts two moves from player0' do
+      newGame.newMove(1, 0)
+      newGame.newMove(1, 0)
       expect(newGame.board[1][0]).to eq(0)
       expect(newGame.board[1][1]).to eq(0)
     end
-    it'accepts two moves from player 1 and player 2'do
-      newGame.newMove(1,0)
-      newGame.newMove(1,1)
+    it 'accepts two moves from player 1 and player 2' do
+      newGame.newMove(1, 0)
+      newGame.newMove(1, 1)
       expect(newGame.board[1][0]).to eq(0)
       expect(newGame.board[1][1]).to eq(1)
     end
     it 'give error when input is not valid ' do
-    expect{newGame.newMove(9,0)}.to output(a_string_including("not valid")).to_stdout
+      expect { newGame.newMove(9, 0) }.to output(a_string_including('not valid')).to_stdout
     end
   end
-  
+
   describe '#validMove?' do
-    subject(:game){described_class.new}
-    it 'rejects move when column is full 'do 
-      7.times{game.newMove(0,0)}
-      expect(game.validMove?(0)).to be false 
+    subject(:game) { described_class.new }
+    it 'rejects move when column is full ' do
+      7.times { game.newMove(0, 0) }
+      expect(game.validMove?(0)).to be false
     end
     it 'reject move when column out of bound' do
       expect(game.validMove?(9)).to be false
     end
     it 'return true for a valid move ' do
-    expect(game.validMove?(3)).to be true
+      expect(game.validMove?(3)).to be true
     end
   end
-  describe '#display'do
-    subject(:newgame){described_class.new}
+  describe '#display' do
+    subject(:newgame) { described_class.new }
     it 'displays the board with the players input' do
-    expect{newgame.display}.to output(a_string_including('O')).to_stdout
+      expect { newgame.display }.to output(a_string_including('O')).to_stdout
     end
     it 'displays the moves for player 0' do
-      newgame.newMove(0,0)
-      expect{newgame.display}.to output(a_string_including('0')).to_stdout
+      newgame.newMove(0, 0)
+      expect { newgame.display }.to output(a_string_including('0')).to_stdout
     end
-    it 'displays the moves for player 1'do
-      newgame.newMove(1,1)
-      expect{newgame.display}.to output(a_string_including('1')).to_stdout
+    it 'displays the moves for player 1' do
+      newgame.newMove(1, 1)
+      expect { newgame.display }.to output(a_string_including('1')).to_stdout
     end
     it 'display the moves for player 1 and 0' do
-      newgame.newMove(0,0)     
-      newgame.newMove(1,1)
-    expect{newgame.display}.to output(a_string_including('1')).to_stdout
-    expect{newgame.display}.to output(a_string_including('0')).to_stdout
+      newgame.newMove(0, 0)
+      newgame.newMove(1, 1)
+      expect { newgame.display }.to output(a_string_including('1')).to_stdout
+      expect { newgame.display }.to output(a_string_including('0')).to_stdout
     end
   end
   describe '#hasWon' do
-    subject(:newgame){described_class.new}
+    subject(:newgame) { described_class.new }
     it 'checks if vertical win has been found ' do
-    newgame.newMove(0,0)
-    newgame.newMove(0,0)
-    newgame.newMove(0,0)
-    newgame.newMove(0,0)
-    expect(newgame.verticalWin(0)).to be true
+      newgame.newMove(0, 0)
+      newgame.newMove(0, 0)
+      newgame.newMove(0, 0)
+      newgame.newMove(0, 0)
+      expect(newgame.verticalWin(0)).to be true
     end
     it 'checks if horizontal win has been found' do
-    newgame.newMove(0,0)
-    newgame.newMove(1,0)
-    newgame.newMove(2,0)
-    newgame.newMove(3,0)
+      newgame.newMove(0, 0)
+      newgame.newMove(1, 0)
+      newgame.newMove(2, 0)
+      newgame.newMove(3, 0)
       newgame.display
-    expect(newgame.horizontalWin(0)).to be true
+      expect(newgame.horizontalWin(0)).to be true
     end
-    it 'check for the diagonal win' do 
-    newgame.newMove(0,0)
-    newgame.newMove(1,1)
-    newgame.newMove(1,0)
-    newgame.newMove(2,2)
-      newgame.newMove(3,3)
-    newgame.newMove(3,2)
-    newgame.newMove(3,1)
-      newgame.newMove(3,0)
-     end
-    it 'checks for the winning condition  '
+    it 'check for the diagonal win' do
+      newgame.newMove(0, 0)
+      newgame.newMove(1, 1)
+      newgame.newMove(1, 0)
+      newgame.newMove(2, 2)
+      newgame.newMove(3, 3)
+      newgame.newMove(3, 2)
+      newgame.newMove(3, 1)
+      newgame.newMove(3, 0)
+    end
   end
   it 'declares who wins'
-  it 'shows winning message to the winner' 
+  it 'shows winning message to the winner'
 end
